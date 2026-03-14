f73099656fba96451bc6d7997e3f9606e3b31a27
mohammadsulaeman <sulaemanm991@gmail.com>
2026-03-14 21:59:39 +0700

    fav update

---
+++ DTO/FavDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+   public class FavDTO
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Please fill Title area")]
+        public string Title { get; set; }
+
+        public string Fav { get; set; }
+
+        public string Logo { get; set; }
+
+        [Display(Name ="Logo Image")]
+        public HttpPostedFileBase LogoImage { get; set; }
+        [Display(Name = "Fav Image")]
+        public HttpPostedFileBase FavImage { get; set; }
+    }
+}
---
+++ UI/Areas/Admin/Controllers/FavController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class FavController : Controller
+    {
+        // GET: Admin/Fav
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
